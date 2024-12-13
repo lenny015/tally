ce2db5234377c02dc5ab6daca356b5a4bee8f185
lenny015 <mightylenny5@gmail.com>
2024-12-12 23:09:53 -0500

    Initial commit

---
+++ src/bot.py
@@ -0,0 +1,109 @@
+import os
+import re
+import discord
+from discord.ext import commands
+from database import db_init, insert_guild, get_current_number, update_channel, update_user_count, get_leaderboard, validate_channels, delete_channel, check_count_channel
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = str(os.getenv('BOT_TOKEN'))
+
+db_init()
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+intents.message_content = True
+intents.members = True
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} with an ID of {bot.user.id}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
+    
+    validate_channels(bot)
+    
+@bot.event
+async def on_guild_channel_delete(channel):
+    if check_count_channel(str(channel.id)):
+        delete_channel(str(channel.id))
+    
+
+@bot.tree.command(name="create-channel", description="Create a new moderated counting channel")
+async def create_channel(interaction: discord.Interaction, name: str = "counting"):
+    await interaction.response.defer()
+    guild = interaction.guild
+    category = interaction.channel.category
+    
+    try:
+        channel = await guild.create_text_channel(name=name, category=category, slowmode_delay=300)
+        insert_guild(str(guild.id), str(channel.id))
+        update_channel(channel.id, 1)
+        await interaction.followup.send(f"Created channel {channel.mention}")
+    except Exception as e:
+        await interaction.followup.send(f"Failed to create channel: `{e}`")
+        
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    
+    channel_id = str(message.channel.id)
+    guild_id = str(message.guild.id)
+    
+    current_num = get_current_number(channel_id)
+    if current_num is not None:
+        match = re.match(r'^(\d+)(?:\s.*)?$', message.content)
+        if match:
+            num = int(match.group(1))
+            
+            if num == current_num:
+                await message.add_reaction("✅")
+                update_channel(channel_id, current_num + 1)
+                update_user_count(str(message.author.id), guild_id, channel_id)
+            else:
+                await message.add_reaction("❌")
+             
+    await bot.process_commands(message)
+    
+@bot.tree.command(name="leaderboard", description="Show the leaderboard for the most active counters")
+async def leaderboard(interaction: discord.Interaction):
+    
+    guild_id = str(interaction.guild.id)
+    leaderboard = get_leaderboard(guild_id)
+    
+    if not leaderboard:
+        await interaction.response.send_message("No users have counted yet.")
+    else:
+        em = discord.Embed(
+            title =  f'Top 10 Counters in {interaction.guild.name}',
+            color=discord.Color.blue()
+        )
+        
+        index = 1
+        medals = [':first_place:', ':second_place:', ':third_place:']
+        
+        for user_id, count in leaderboard:
+            member = interaction.guild.get_member(int(user_id))
+            
+            if member:
+                name = member.display_name
+                
+                if 1 <= index <= 3:
+                    em.add_field(name=f"{medals[index-1]}`{name}` `{count}`", value="", inline=False)
+                else:
+                    em.add_field(name=f"`#{index}` `{name}` `{count}`", value="", inline=False)
+            
+            if index == 10:
+                break
+            index += 1
+        
+        await interaction.response.send_message(embed=em)
+    
+    
+if __name__ == "__main__":
+    bot.run(TOKEN)
---
+++ src/database.py
@@ -0,0 +1,173 @@
+import sqlite3
+import os
+from discord.ext.commands import Bot
+
+DB_DIR = os.path.join("db")
+DB_PATH = os.path.join("db", "database.db")
+
+def db_init():
+    os.makedirs(DB_DIR, exist_ok=True)
+    
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+
+        cursor.execute("""
+                        CREATE TABLE IF NOT EXISTS guilds (
+                            guild_id TEXT PRIMARY KEY,
+                            counting_channel_id TEXT
+                        )
+                    """)
+        
+        cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS users (
+                        user_id TEXT,
+                        guild_id TEXT,
+                        channel_id TEXT,
+                        counted_numbers INTEGER DEFAULT 0,
+                        PRIMARY KEY (user_id, guild_id, channel_id),
+                        FOREIGN KEY (guild_id) REFERENCES guilds(guild_id)
+                    )
+                    """)
+        
+        cursor.execute("""
+                    CREATE TABLE IF NOT EXISTS channels (
+                            channel_id TEXT PRIMARY KEY,
+                            guild_id TEXT,
+                            current_number INTEGER DEFAULT 1
+                        )
+                    """)
+        
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(f"Database connection error: {e}")
+        raise
+    
+    
+    
+def insert_guild(guild_id, channel_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   INSERT OR REPLACE INTO guilds (guild_id, counting_channel_id) VALUES (?, ?)
+                   """, (guild_id, channel_id))
+    
+    conn.commit()
+    conn.close()
+
+def get_current_number(channel_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   SELECT current_number FROM channels WHERE channel_id = ?
+                   """, (channel_id,))
+    result = cursor.fetchone()
+    conn.close()
+    
+    if result:
+        return result[0]
+    else:
+        return None
+    
+def update_channel(channel_id, next_num):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   INSERT OR REPLACE INTO channels (channel_id, guild_id, current_number)
+                   VALUES (?, (SELECT guild_id FROM guilds where counting_channel_id = ?), ?)
+                   """, (channel_id, channel_id, next_num))
+    
+    conn.commit()
+    conn.close()
+    
+def update_user_count(user_id, guild_id, channel_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   INSERT INTO users (user_id, guild_id, channel_id, counted_numbers) VALUES (?, ?, ?,1)
+                   ON CONFLICT(user_id, guild_id, channel_id) DO UPDATE SET counted_numbers = counted_numbers + 1
+                   """, (user_id, guild_id, channel_id))
+    
+    conn.commit()
+    conn.close()
+    
+def get_leaderboard(guild_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   SELECT user_id, counted_numbers FROM users WHERE guild_id = ? ORDER BY counted_numbers DESC
+                   """, (guild_id,))
+    
+    result = cursor.fetchall()
+    conn.close()
+    
+    return result
+
+def validate_channels(bot: Bot):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   SELECT channel_id, guild_id FROM channels
+                   """)
+    channels = cursor.fetchall()
+    
+    for channel_id, guild_id in channels:
+        try:
+            guild = bot.get_guild(int(guild_id))
+            if not guild or not guild.get_channel(int(channel_id)):
+                cursor.execute("""
+                               DELETE FROM channels WHERE channel_id = ?
+                               """,(channel_id,))
+                cursor.execute("""
+                               DELETE FROM guilds WHERE counting_channel_id = ?
+                               """,(channel_id,))
+                cursor.execute("""
+                               DELETE FROM users WHERE channel_id = ?
+                               """,(channel_id,))
+                print(f"{channel_id} not in server")
+        except Exception as e:
+            print(f"Channel validation error on channel ID {channel_id}: {e}")
+    
+    conn.commit()
+    conn.close()
+    
+def delete_channel(channel_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   DELETE FROM channels WHERE channel_id = ?
+                   """,(channel_id,))
+    
+    cursor.execute("""
+                   DELETE FROM guilds WHERE counting_channel_id = ? 
+                   """,(channel_id,))
+    
+    cursor.execute("""
+                   DELETE FROM users WHERE channel_id = ?
+                   """,(channel_id,))
+    
+    conn.commit()
+    conn.close()
+    
+def check_count_channel(channel_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   SELECT channel_id FROM channels WHERE channel_id = ?
+                   """,(channel_id,))
+    
+    print(f"Deleting channel {channel_id}")
+    
+    result = cursor.fetchone()
+    conn.close()
+    
+    return result
